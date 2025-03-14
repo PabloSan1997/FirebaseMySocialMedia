5c4b1a07c8cd539db518789644b1bd0b5001ad66
PabloSan1997 <jos_pab_997@hotmali.com>
2025-03-13 18:17:50 -0600

    added interfaes typescript

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,93 @@
 /// <reference types="vite/client" />
 
+//-------User intarface
+
+interface LoginDto{
+    username:string;
+    password:string;
+}
+
+interface RegisterDto{
+    username:string;
+    password:string;
+    fullname:string;
+}
+
+interface UserInfo{
+    username:string;
+    fullname:string;
+    description:string;
+    urlImage:string;
+    born:string;
+    createCount:string;
+}
+
+interface UserHeader{
+    username:string;
+    fullname:string;
+    urlImage:string;
+}
+
+interface ViewFollow{
+    viewFollow:boolean
+}
+
+interface TokenDto{
+    username:string;
+    token:string;
+}
+
+interface SaveUserInfo{
+    description:string;
+    born:string
+}
+
+interface UserPictureResponse{
+    urlImage:string;
+}
+
+interface ViewFollowCount{
+    followings:number;
+    followers:number;
+}
+
+//---------interaction interface
+
+interface OneImagenInterface{
+    id:number;
+    urlImage:string;
+    description:string;
+    likes:number;
+    comments:CommentInterface[],
+    userLike:boolean;
+    user:UserHeader
+}
+
+
+
+interface ImagenInterface{
+    id:number;
+    urlImage:string;
+    description:string;
+    likes:number;
+    comments:number;
+    userLike:boolean;
+    user:UserHeader
+}
+
+interface CommentInterface {
+    id:number;
+    comment:string;
+    createAt:string;
+    user:UserHeader
+}
+
+interface SaveComment{
+    comment:string;
+}
+
+
+interface LikeResponse{
+    idImage:number;
+    userLike:boolean
+}
